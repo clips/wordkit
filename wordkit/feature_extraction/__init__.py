683da93df1561276998b78d1170078f6874537ab
stephantul <stephan.tulkens@uantwerpen.be>
2018-02-28 14:13:29 +0100

    Add consonant vowel separation for characters

--- wordkit/feature_extraction/__init__.py
+++ wordkit/feature_extraction/__init__.py
@@ -1,10 +1,13 @@
 """Character and phoneme featurizers."""
-from .orthography import one_hot_characters
+from .orthography import one_hot_characters, consonant_vowel_characters
 from .phonology import extract_grouped_phoneme_features
 from .phonology import extract_phoneme_features
 from .phonology import predefined_features
+from .phonology import extract_one_hot_phonemes
 
 __all__ = ["one_hot_characters",
+           "consonant_vowel_characters",
+           "extract_one_hot_phonemes",
            "extract_grouped_phoneme_features",
            "extract_phoneme_features",
            "predefined_features"]
--- wordkit/feature_extraction/orthography.py
+++ wordkit/feature_extraction/orthography.py
@@ -2,6 +2,11 @@
 import numpy as np
 
 
+VOWELS = set("aeioujy")
+CONSONANTS = set("bcdfghklmnpqrstvwxz")
+BOTH = CONSONANTS.union(VOWELS)
+
+
 def one_hot_characters(characters):
     """
     Extract one-hot encoded binary vectors for your characters.
@@ -24,3 +29,39 @@ def one_hot_characters(characters):
     binary_features = np.eye(len(characters))
     return {l: binary_features[idx]
             for idx, l in enumerate(characters)}
+
+
+def consonant_vowel_characters(characters):
+    """
+    Split sets of characters into consonant and vowel character sets.
+
+    This is useful for testing whether characters themselves are already
+    categorized into some kind of Consonant Vowel set, or are already split
+    into syllables.
+
+    Note that this functions throws an error if a character is neither a
+    consonant nor a vowel.
+
+    Parameters
+    ----------
+    characters : string or list
+        The unique characters occuring in your dataset.
+
+    Returns
+    -------
+    vowels : list
+        The list of characters which are vowels.
+    consonants : list
+        The list of characters which are consonants.
+
+    """
+    diff = set([x.lower() for x in characters]) - BOTH
+
+    if diff:
+        raise ValueError("{} was in your character set, but is not a vowel or"
+                         " consonant.".format(diff))
+
+    vowels = [x for x in characters if x.lower() in VOWELS]
+    consonants = [x for x in characters if x.lower() in CONSONANTS]
+
+    return vowels, consonants
