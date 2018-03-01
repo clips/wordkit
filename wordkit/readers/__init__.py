a160184c37b4872babfc8652591c6e35287939a9
stephantul <stephan.tulkens@uantwerpen.be>
2018-03-01 13:48:52 +0100

    Add corpusaugmenter
    CorpusAugmenter can be used to augment corpora with information from
    other sources. This allows us to use the Celex corpora with better
    sources of frequency information, such as the SUBTLEX frequency norms,
    while still keeping the phonology information from CELEX

--- wordkit/readers/__init__.py
+++ wordkit/readers/__init__.py
@@ -2,5 +2,6 @@
 from .celex import Celex
 from .cmudict import CMU
 from .deri import Deri
+from .corpusaugmenter import CorpusAugmenter
 
-__all__ = ["Celex", "CMU", "Deri"]
+__all__ = ["Celex", "CMU", "Deri", "CorpusAugmenter"]
