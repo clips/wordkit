2ff1a46deaaa2e09f22d8cffa5d4c07845fdae85
stephantul <stephan.tulkens@uantwerpen.be>
2018-03-13 11:53:32 +0100

    some formatting

--- wordkit/feature_extraction/__init__.py
+++ wordkit/feature_extraction/__init__.py
@@ -1,9 +1,9 @@
 """Character and phoneme featurizers."""
 from .orthography import one_hot_characters, consonant_vowel_characters
-from .phonology import extract_grouped_phoneme_features
-from .phonology import extract_phoneme_features
-from .phonology import predefined_features
-from .phonology import extract_one_hot_phonemes
+from .phonology import extract_grouped_phoneme_features, \
+                       extract_phoneme_features, \
+                       predefined_features, \
+                       extract_one_hot_phonemes
 
 __all__ = ["one_hot_characters",
            "consonant_vowel_characters",
