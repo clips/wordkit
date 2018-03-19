08ed67a80f9e0dd69229a32ad624f256807f7ce6
stephantul <stephan.tulkens@uantwerpen.be>
2018-03-19 16:18:23 +0100

    added functions for extracting all symbols from a specified field

--- wordkit/feature_extraction/__init__.py
+++ wordkit/feature_extraction/__init__.py
@@ -1,13 +1,16 @@
 """Character and phoneme featurizers."""
-from .orthography import one_hot_characters, consonant_vowel_characters
+from .orthography import extract_one_hot_characters, \
+                         extract_consonant_vowel_characters
 from .phonology import extract_grouped_phoneme_features, \
                        extract_phoneme_features, \
                        predefined_features, \
                        extract_one_hot_phonemes
+from .general import extract_characters
 
-__all__ = ["one_hot_characters",
-           "consonant_vowel_characters",
+__all__ = ["extract_one_hot_characters",
+           "extract_consonant_vowel_characters",
            "extract_one_hot_phonemes",
            "extract_grouped_phoneme_features",
            "extract_phoneme_features",
-           "predefined_features"]
+           "predefined_features",
+           "extract_characters"]
