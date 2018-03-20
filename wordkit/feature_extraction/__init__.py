343a1f00ec402dfd69b88269a026feaf93cc5bf7
stephantul <stephan.tulkens@uantwerpen.be>
2018-03-20 09:06:44 +0100

    Rewrote feature extraction

--- wordkit/feature_extraction/__init__.py
+++ wordkit/feature_extraction/__init__.py
@@ -1,16 +1,12 @@
 """Character and phoneme featurizers."""
-from .orthography import extract_one_hot_characters, \
-                         extract_consonant_vowel_characters
-from .phonology import extract_grouped_phoneme_features, \
-                       extract_phoneme_features, \
-                       predefined_features, \
-                       extract_one_hot_phonemes
-from .general import extract_characters
+from .orthography import OneHotCharacterExtractor
+from .phonology import OneHotPhonemeExtractor, \
+                       PhonemeFeatureExtractor, \
+                       PredefinedFeatureExtractor, \
+                       GroupedPhonemeFeatureExtractor
 
-__all__ = ["extract_one_hot_characters",
-           "extract_consonant_vowel_characters",
-           "extract_one_hot_phonemes",
-           "extract_grouped_phoneme_features",
-           "extract_phoneme_features",
-           "predefined_features",
-           "extract_characters"]
+__all__ = ["OneHotCharacterExtractor",
+           "OneHotPhonemeExtractor",
+           "PhonemeFeatureExtractor",
+           "PredefinedFeatureExtractor",
+           "GroupedPhonemeFeatureExtractor"]
--- wordkit/feature_extraction/orthography.py
+++ wordkit/feature_extraction/orthography.py
@@ -1,67 +1,13 @@
 """Functions for extracting orthographical features."""
 import numpy as np
 
-
-VOWELS = set("aeioujy")
-CONSONANTS = set("bcdfghklmnpqrstvwxz")
-BOTH = CONSONANTS.union(VOWELS)
+from .base import BaseExtractor
 
 
-def extract_one_hot_characters(characters):
-    """
-    Extract one-hot encoded binary vectors for your characters.
+class OneHotCharacterExtractor(BaseExtractor):
+    """Convert your characters to one hot features."""
 
-    Each character will be assigned a unique one-hot encoded binary
-    vector. Words transformed using this feature set will thus be
-    matrices of one-hot encoded vectors.
-
-    Parameters
-    ----------
-    characters : string or list
-        The unique characters occurring in your dataset.
-
-    Returns
-    -------
-    features : dict
-        A dictionary mapping from characters to one-hot encoded arrays.
-
-    """
-    binary_features = np.eye(len(characters))
-    return {l: binary_features[idx]
-            for idx, l in enumerate(characters)}
-
-
-def extract_consonant_vowel_characters(characters):
-    """
-    Split sets of characters into consonant and vowel character sets.
-
-    This is useful for testing whether characters themselves are already
-    categorized into some kind of Consonant Vowel set, or are already split
-    into syllables.
-
-    Note that this functions throws an error if a character is neither a
-    consonant nor a vowel.
-
-    Parameters
-    ----------
-    characters : string or list
-        The unique characters occuring in your dataset.
-
-    Returns
-    -------
-    vowels : list
-        The list of characters which are vowels.
-    consonants : list
-        The list of characters which are consonants.
-
-    """
-    diff = set([x.lower() for x in characters]) - BOTH
-
-    if diff:
-        raise ValueError("{} was in your character set, but is not a vowel or"
-                         " consonant.".format(diff))
-
-    vowels = [x for x in characters if x.lower() in VOWELS]
-    consonants = [x for x in characters if x.lower() in CONSONANTS]
-
-    return vowels, consonants
+    def _process(self, symbols):
+        """Create one hot encoded features."""
+        features = np.eye(len(symbols), dtype=np.int)
+        return dict(zip(symbols, features))
