f6f7600e1ab6a12c8496850d79a0ec588181453a
stephantul <stephan.tulkens@uantwerpen.be>
2018-04-24 09:12:04 +0200

    add good description, example to character featurizer

--- wordkit/feature_extraction/orthography.py
+++ wordkit/feature_extraction/orthography.py
@@ -5,7 +5,21 @@ from .base import BaseExtractor
 
 
 class OneHotCharacterExtractor(BaseExtractor):
-    """Convert your characters to one hot features."""
+    """
+    Convert your characters to one hot features.
+
+    Note: if your data contains diacritics, such as 'é', you might be better
+    off normalizing these. The OneHotCharacterExtractor will assign these
+    completely dissimilar representations.
+
+    Example
+    -------
+    >>> from wordkit.feature_extraction import OneHotCharacterExtractor
+    >>> words = ["c'est", "stéphan"]
+    >>> o = OneHotCharacterExtractor(field=None)
+    >>> o.extract(words)
+
+    """
 
     def _process(self, symbols):
         """Create one hot encoded features."""
