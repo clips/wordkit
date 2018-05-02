8c89ab845617fc8a7f294eb7ec8f6836fd3a1f89
stephantul <stephan.tulkens@uantwerpen.be>
2018-05-02 12:41:39 +0200

    update setup

--- setup.py
+++ setup.py
@@ -6,14 +6,17 @@ from setuptools import find_packages
 
 
 setup(name='wordkit',
-      version='0.1.0',
+      version='1.0.0',
       description='Word featurization',
       author='Stéphan Tulkens',
       author_email='stephan.tulkens@uantwerpen.be',
-      url='https://github.com/stephantul/wordkit',
-      license='MIT',
+      url='https://github.com/clips/wordkit',
+      license='GPLv3',
       packages=find_packages(exclude=['examples']),
-      install_requires=['numpy>=1.11.0'],
+      install_requires=['numpy>=1.11.0',
+                        'ipapy',
+                        'pandas',
+                        'scikit_learn'],
       classifiers=[
           'Intended Audience :: Developers',
           'Programming Language :: Python :: 3',],
