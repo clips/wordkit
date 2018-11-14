ecc4fe412720c2480b19982c35cf8d65f45e5c13
stephantul <stephan.tulkens@uantwerpen.be>
2018-11-14 12:03:10 +0100

    make merge somewhat more user friendly

--- wordkit/corpora/merge.py
+++ wordkit/corpora/merge.py
@@ -30,13 +30,13 @@ def merge(from_corpus,
     to_corpus : list of dictionaries
         A list of dictionaries. This corpus will be augmented with the
         information from the from_corpus.
-    merge_fields : tuple
+    merge_fields : tuple or string
         The fields to use in determining equivalence between two words in the
-        corpora. For example, if union_fields = ("orthography",), only the
-        orthography field is used to compare words. If union_fields =
+        corpora. For example, if merge_fields = ("orthography",), only the
+        orthography field is used to compare words. If merge_fields =
         ("orthography", "phonology"), both the information in "orthography" and
         "phonology" needs to be equivalent.
-    transfer_fields : tuple
+    transfer_fields : tuple or string
         The fields to transfer from the from_corpus to the to_corpus.
     discard : bool
         If this is set to True, words that are not in both corpora are
@@ -49,11 +49,17 @@ def merge(from_corpus,
     >>> c = Celex("path")
     >>> words_s = s.transform()
     >>> words_c = c.transform()
-    >>> new = merge(words_s, words_c, ("orthography",), ("frequency",))
+    >>> new = merge(words_s, words_c, "orthography", "frequency")
 
     """
     from_keys = set(from_corpus[0].keys())
     to_keys = set(to_corpus[0].keys())
+
+    if isinstance(merge_fields, str):
+        merge_fields = (merge_fields,)
+
+    if isinstance(transfer_fields, str):
+        transfer_fields = (transfer_fields,)
 
     all_fields = set(from_keys) & set(to_keys)
     all_diff = set(merge_fields) - all_fields
