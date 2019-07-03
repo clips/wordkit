e0025d84f352d6abe0a1b89855547594a1608723
stephantul <stephan.tulkens@uantwerpen.be>
2019-07-03 09:06:56 +0200

    remove some old structure

--- wordkit/base/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Base things."""
-from .feature_extraction import BaseExtractor
-from .transformer import BaseTransformer, FeatureTransformer
-
-__all__ = ["BaseExtractor", "BaseTransformer", "FeatureTransformer"]
