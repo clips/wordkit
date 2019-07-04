76196bd924257aaf002df0eb43bc8693d140457c
stephantul <stephan.tulkens@uantwerpen.be>
2019-07-04 14:45:37 +0200

    fix hashes

--- wordkit/corpora/corpora/lexiconproject.py
+++ wordkit/corpora/corpora/lexiconproject.py
@@ -1,4 +1,5 @@
 """Readers for lexicon project corpora."""
+import os
 from ..base import reader
 from ..base.utils import _calc_hash
 
@@ -24,19 +25,19 @@ PROJECT2FIELD = {"dlp1": {"orthography": "spelling"},
                          "rt": "Stim_RT_M"}}
 AUTO_PROJECT = {"french lexicon project words.xls": "flp",
                 "blp-items.txt": "blp",
-                "dlp-items.txt": "dlp",
+                "dlp-items.txt": "dlp1",
                 "dlp2_items.tsv": "dlp2",
                 "elp-items.csv": "elp",
                 "chinese lexicon project sze et al.csv": "clp",
                 "klp_ld_item_ver.1.0.xlsx": "klp"}
 
-HASHES = {"93eda69ab4479fbdcce271f6d49fd953": "blp",
-          "7c446dbd6f9a9e30be713b30b8c896c8": "clp",
-          "fd604022fa808096610b6a2d60680589": "dlp",
-          "ee807ec2b01e269ae24e948d47f37574": "dlp2",
-          "7c23effdd68df5a29d0662390c6f4f02": "elp",
-          "3840e2b07d6ea66336cce00c404007ab": "flp",
-          "08b7aedc18ee846eccfce0df1b83e97b": "klp"}
+HASHES = {'4556a72ec2a4907a69ea3fdd2343ddf7': 'klp',
+          '7c23effdd68df5a29d0662390c6f4f02': 'elp',
+          '7c446dbd6f9a9e30be713b30b8c896c8': 'clp',
+          '93eda69ab4479fbdcce271f6d49fd953': 'blp',
+          '96da0efa380cccfbbd65c7baa16acd62': 'flp',
+          'ee807ec2b01e269ae24e948d47f37574': 'dlp2',
+          'fd604022fa808096610b6a2d60680589': 'dlp1'}
 
 
 def lexiconproject(path,
@@ -44,10 +45,19 @@ def lexiconproject(path,
                    language=None,
                    project=None):
     if project is None:
-        hash = _calc_hash(path)
-        project = HASHES[hash]
+        try:
+            hash = _calc_hash(path)
+            project = HASHES[hash]
+        except KeyError:
+            try:
+                project = AUTO_PROJECT[os.path.split(path)[-1]]
+            except KeyError:
+                raise ValueError("Your project is not correct. Allowed "
+                                 f"projects are {set(PROJECT2FIELD.keys())}")
     else:
-        if project not in PROJECT2FIELD:
+        try:
+            project = AUTO_PROJECT[os.path.split(path)[-1]]
+        except KeyError:
             raise ValueError("Your project is not correct. Allowed "
                              f"projects are {set(PROJECT2FIELD.keys())}")
     if language is None:
