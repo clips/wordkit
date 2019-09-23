c25089245358ad0dd82e933d30da18115ac71e8a
stephantul <stephan.tulkens@uantwerpen.be>
2019-09-23 08:33:20 +0200

    add new subtlex readers

--- wordkit/corpora/corpora/subtlex.py
+++ wordkit/corpora/corpora/subtlex.py
@@ -1,73 +1,30 @@
 """Corpus readers for Subtlex."""
-import os
 from ..base import reader
-from ..base.utils import _calc_hash
 
-PROJECT2FIELD = {"subtlex-uk": {"orthography": "Spelling",
-                                "frequency": "FreqCount"},
-                 "subtlex-us": {"orthography": "Word",
-                                "frequency": "FREQcount"},
-                 "subtlex-nl": {"orthography": "Word",
-                                "frequency": "FREQcount"},
-                 "subtlex-de": {"orthography": "Word",
-                                "frequency": "WFfreqCount"},
-                 "subtlex-ch": {"orthography": "Word",
-                                "frequency": "WCount"}}
+LANG2FIELD = {"eng-uk": {"orthography": "Spelling",
+                         "frequency": "FreqCount"},
+              "eng-us": {"orthography": "Word",
+                         "frequency": "FREQcount"},
+              "nld": {"orthography": "Word",
+                      "frequency": "FREQcount"},
+              "deu": {"orthography": "Word",
+                      "frequency": "WFfreqCount"},
+              "zh": {"orthography": "Word",
+                     "frequency": "WCount"}}
 
-PROJECT2SEP = {"subtlex-uk": ",",
-               "subtlex-nl": "\t",
-               "subtlex-ch": ",",
-               "subtlex-de": ",",
-               "subtlex-us": ","}
-
-AUTO_LANGUAGE = {'subtlex-ch': 'chi',
-                 'subtlex-de': 'deu',
-                 'subtlex-nl': 'nld',
-                 'subtlex-uk': 'eng-uk',
-                 'subtlex-us': 'eng-us'}
-
-AUTO_PROJECT = {"SUBTLEX-CH-WF.xlsx": "subtlex-chi",
-                "SUBTLEX-UK.xlsx": "subtlex-uk",
-                "SUBTLEX-NL.cd-above2.txt": "subtlex-nl",
-                "SUBTLEX-DE cleaned with Google00 frequencies.xlsx": "subtlex-de", # noqa
-                "SUBTLEXusfrequencyabove1.xls": "subtlex-us"}
-
-
-HASHES = {'786e0d7d03f9ce93b26577b543c9eb0f': 'subtlex-nl',
-          '79c8672068fcf9b02e4080862c9eb17b': 'subtlex-ch',
-          'b528ba6be0785aecda7ac893caf1e722': 'subtlex-us',
-          'd171a587c9355ffb7ff5ed2ddfe55e89': 'subtlex-de',
-          'f252d192d592812f9e5bd0b4a68fffdf': 'subtlex-uk'}
+LANG2SEP = {"eng-uk": ",",
+            "nld": "\t",
+            "zh": ",",
+            "deu": ",",
+            "eng-us": ","}
 
 
 def subtlex(path,
             fields=("orthography", "frequency"),
-            language=None,
-            project=None):
+            language=None):
     """Initialize the subtlex reader."""
-    if project is None:
-        try:
-            hash = _calc_hash(path)
-            project = HASHES[hash]
-        except KeyError:
-            try:
-                project = AUTO_PROJECT[os.path.split(path)[-1]]
-            except KeyError:
-                raise ValueError("Your project is not correct. Allowed "
-                                 f"projects are {set(PROJECT2FIELD.keys())}")
-    else:
-        try:
-            project = AUTO_PROJECT[os.path.split(path)[-1]]
-        except KeyError:
-            raise ValueError("Your project is not correct. Allowed "
-                             f"projects are {set(PROJECT2FIELD.keys())}")
     if language is None:
-        try:
-            language = AUTO_LANGUAGE[project]
-        except KeyError:
-            raise ValueError("You passed None to language, but we failed "
-                             "to determine the language automatically.")
-
+        raise ValueError("Please pass a language")
     if language == "chi":
         skiprows = 2
     else:
@@ -75,7 +32,32 @@ def subtlex(path,
 
     return reader(path,
                   fields,
-                  PROJECT2FIELD[project],
+                  LANG2FIELD[language],
                   language,
-                  sep=PROJECT2SEP[project],
+                  sep=LANG2SEP[language],
                   skiprows=skiprows)
+
+
+def subtlexuk(path,
+              fields=("orthography", "frequency")):
+    return subtlex(path, fields, "eng-uk")
+
+
+def subtlexus(path,
+              fields=("orthography", "frequency")):
+    return subtlex(path, fields, "eng-us")
+
+
+def subtlexnl(path,
+              fields=("orthography", "frequency")):
+    return subtlex(path, fields, "eng-us")
+
+
+def subtlexde(path,
+              fields=("orthography", "frequency")):
+    return subtlex(path, fields, "ger")
+
+
+def subtlexzh(path,
+              fields=("orthography", "frequency")):
+    return subtlex(path, fields, "zh")
