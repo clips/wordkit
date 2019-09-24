122c12b8e792f10e88274f5176078ee58d618f7f
stephantul <stephan.tulkens@uantwerpen.be>
2019-09-24 15:47:09 +0200

    add initialization

--- wordkit/features/holograph/__init__.py
+++ wordkit/features/holograph/__init__.py
@@ -1,1 +1,15 @@
 """Holographic features."""
+from .kanerva import (KanervaLinearTransformer,
+                      KanervaWickelTransformer,
+                      KanervaOpenNGramTransformer)
+from .plate import (PlateLinearTransformer,
+                    PlateWickelTransformer,
+                    PlateOpenNGramTransformer)
+
+
+__all__ = ["KanervaLinearTransformer",
+           "KanervaWickelTransformer",
+           "KanervaOpenNGramTransformer",
+           "PlateLinearTransformer",
+           "PlateWickelTransformer",
+           "PlateOpenNGramTransformer"]
